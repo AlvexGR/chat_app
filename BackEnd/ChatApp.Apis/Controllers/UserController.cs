e7e4f3153c54e48f5055475f945e51d8e603c18f
Nhan Nguyen <nhannn@imt-soft.com>
2020-09-13 09:44:54 +0700

    [BE] Add UserController

---
+++ BackEnd/ChatApp.Apis/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Apis.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
