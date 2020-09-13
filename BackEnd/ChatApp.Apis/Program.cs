6e1926b63e2c5601257d545a124e16e96f015570
Nhan Nguyen <nhannn@imt-soft.com>
2020-09-13 12:32:54 +0700

    [BE] Refactor config

--- BackEnd/ChatApp.Apis/Program.cs
+++ BackEnd/ChatApp.Apis/Program.cs
@@ -28,7 +28,6 @@ namespace ChatApp.Apis
                                 fileSizeLimitBytes: 50000000,
                                 rollOnFileSizeLimit: true),
                     writeToProviders: true)
-
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- BackEnd/ChatApp.Apis/Startup.cs
+++ BackEnd/ChatApp.Apis/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ChatApp.Apis
 {
